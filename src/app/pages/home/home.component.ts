e03e5c864f7faf75eb694e6a0e6f736afc4dcb44
RuthAnn <adorableruthann@gmail.com>
2026-08-23 12:39:33 +0200

    latest

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -19,7 +19,7 @@ import { RouterLink } from '@angular/router';
 })
 export class HomeComponent {
 
-  banner!: HomepageBanner;
+  banner: HomepageBanner | null = null;; 
   homePageService = inject(HomepageService);
 
   constructor(private translate: TranslateService) { }
