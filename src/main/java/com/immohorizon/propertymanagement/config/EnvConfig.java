a572a43b4a858c9b7756b7338f1745d913c4922e
RuthAnn <adorableruthann@gmail.com>
2026-09-19 04:05:23 +0200

    dotenvdeleted

--- src/main/java/com/immohorizon/propertymanagement/config/EnvConfig.java
+++ src/main/java/com/immohorizon/propertymanagement/config/EnvConfig.java
@@ -4,9 +4,7 @@ import io.github.cdimascio.dotenv.Dotenv;
 
 public class EnvConfig {
 
-    private static final Dotenv dotenv = Dotenv.load();
-
     public static String get(String key) {
-        return dotenv.get(key);
+        return System.getenv(key);
     }
 }
